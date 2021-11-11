37c6c335aaaf5b236efc41f482f26ed1275e90c5
Juan-Ilhan <72693148+Juan-Ilhan@users.noreply.github.com>
2021-11-11 20:56:18 +0100

    Ressource angelegt(Rest API)

--- src/main/java/htw/berlin/webtech/service/PersonService.java
+++ src/main/java/htw/berlin/webtech/service/PersonService.java
@@ -3,6 +3,7 @@ package htw.berlin.webtech.service;
 import htw.berlin.webtech.persistence.PersonEntity;
 import htw.berlin.webtech.persistence.PersonRepository;
 import htw.berlin.webtech.web.api.Person;
+import htw.berlin.webtech.web.api.PersonCreateRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,13 +21,27 @@ public class PersonService {
 
         List<PersonEntity> persons = personRepository.findAll();
         return persons.stream()
-                .map(personEntity -> new Person(
-                        personEntity.getId(),
-                        personEntity.getFirstName(),
-                        personEntity.getLastName(),
-                        personEntity.isVaccinated()
-                ))
+                .map(this::transformEntity)
                 .collect(Collectors.toList());
 
+    }
+
+    public Person create(PersonCreateRequest request){
+
+        var personEntity = new PersonEntity(request.getFirstName(), request.getLastName(), request.isVaccinated());
+
+        personEntity = personRepository.save(personEntity);
+        return transformEntity(personEntity);
+    }
+
+    private Person transformEntity(PersonEntity personEntity){
+
+        return new Person(
+
+                personEntity.getId(),
+                personEntity.getFirstName(),
+                personEntity.getLastName(),
+                personEntity.isVaccinated()
+        );
     }
 }
--- src/main/java/htw/berlin/webtech/web/PersonRestController.java
+++ src/main/java/htw/berlin/webtech/web/PersonRestController.java
@@ -3,10 +3,15 @@ package htw.berlin.webtech.web;
 import htw.berlin.webtech.persistence.PersonRepository;
 import htw.berlin.webtech.service.PersonService;
 import htw.berlin.webtech.web.api.Person;
+import htw.berlin.webtech.web.api.PersonCreateRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -26,6 +31,13 @@ public class PersonRestController {
     public ResponseEntity<List<Person>> fetchPersons() {
         return ResponseEntity.ok(personService.findAll());
 
+    }
+
+    @PostMapping(path = "/api/v1/persons")
+    public ResponseEntity<Void> createPerson(@RequestBody PersonCreateRequest request) throws URISyntaxException {
+      var person = personService.create(request);
+        URI uri = new URI("/api/v1/persons/" + person.getId());
+      return ResponseEntity.created(uri).build();
     }
 
 }
