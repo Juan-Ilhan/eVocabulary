d7ae944e1e1272be498e4f6ab02008b97b87dba5
Juan-Ilhan <72693148+Juan-Ilhan@users.noreply.github.com>
2021-11-11 21:12:06 +0100

    Bestimmte Person Abfrage

--- src/main/java/htw/berlin/webtech/web/PersonRestController.java
+++ src/main/java/htw/berlin/webtech/web/PersonRestController.java
@@ -5,10 +5,7 @@ import htw.berlin.webtech.service.PersonService;
 import htw.berlin.webtech.web.api.Person;
 import htw.berlin.webtech.web.api.PersonCreateRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -32,6 +29,19 @@ public class PersonRestController {
         return ResponseEntity.ok(personService.findAll());
 
     }
+
+    @GetMapping(path = "/api/v1/persons/{id}")
+    public ResponseEntity<Person> fetchPersonById(@PathVariable Long id){
+
+        var person = personService.findById(id);
+        return person != null? ResponseEntity.ok(person) : ResponseEntity.notFound().build();
+
+
+    }
+
+
+
+
 
     @PostMapping(path = "/api/v1/persons")
     public ResponseEntity<Void> createPerson(@RequestBody PersonCreateRequest request) throws URISyntaxException {
